7170385ac33e7fdcaf187796b69b065d8d33edb6
Ayush <Ayush@192.168.1.7>
2015-07-31 22:24:08 +0545

    from mac DiceRoller

---
+++ NetBeans-master/src/Ludo.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Ludo {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        Random dice=new Random();
+        System.out.println("Enter Number of players");
+        int n=inp.nextInt();
+        int score[]=new int[n];
+        String[] name=new String[20];
+        
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the "+(i)+ " player's name");
+            name[i]=inp.nextLine();
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(name[i]+" hit enter to roll the dice");
+            String check=inp.nextLine();
+            score[i]=dice.nextInt(6);
+            score[i]++;
+           
+        }
+        
+        for(int i=0;i<n;i++){
+            
+            System.out.println("\n"+name[i]+" Score= "+score[i]);
+        }
+        
+    }
+    
+}
